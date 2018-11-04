4c0da9b84c48f93c5674729de0c440a614b5b1ab
jackwisdom <Jackwisdom143@gmail.com>
2018-11-04 15:39:47 +0800

    added changepassword cmd

---
+++ src/main/java/git/jackwisdom/sponge/securelogin/cmd/executor/ChangePasswordCommand.java
@@ -0,0 +1,13 @@
+package git.jackwisdom.sponge.securelogin.cmd.executor;
+
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+
+public class ChangePasswordCommand extends ICmdExecutor {
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        return null;
+    }
+}
