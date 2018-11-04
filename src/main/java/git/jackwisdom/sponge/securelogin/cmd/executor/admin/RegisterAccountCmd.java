3bf6ebf2a862558e96fad4ea5a4a283633d4ec67
jackwisdom <Jackwisdom143@gmail.com>
2018-11-04 15:46:01 +0800

    added admin cmd frame

---
+++ src/main/java/git/jackwisdom/sponge/securelogin/cmd/executor/admin/RegisterAccountCmd.java
@@ -0,0 +1,14 @@
+package git.jackwisdom.sponge.securelogin.cmd.executor.admin;
+
+import git.jackwisdom.sponge.securelogin.cmd.executor.ICmdExecutor;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+
+public class RegisterAccountCmd extends ICmdExecutor {
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        return null;
+    }
+}
