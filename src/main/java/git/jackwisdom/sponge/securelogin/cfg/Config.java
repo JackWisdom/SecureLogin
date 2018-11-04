0961345503fb3309e25757d7ac348ae620c2c92a
jackwisdom <jackwisdom143@gmail.com>
2018-11-04 10:19:36 +0800

    added frame for cfgs

---
+++ src/main/java/git/jackwisdom/sponge/securelogin/cfg/Config.java
@@ -0,0 +1,24 @@
+package git.jackwisdom.sponge.securelogin.cfg;
+
+import com.google.inject.Inject;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
+
+import java.nio.file.Path;
+
+public class Config {
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private Path defaultConfig;
+
+    @Inject
+    @DefaultConfig(sharedRoot = true)
+    private ConfigurationLoader<CommentedConfigurationNode> configManager;
+
+    @Inject
+    @ConfigDir(sharedRoot = false)
+    private Path privateConfigDir;
+}
--- src/main/java/git/jackwisdom/sponge/securelogin/listener/AntiGriefListener.java
+++ src/main/java/git/jackwisdom/sponge/securelogin/listener/AntiGriefListener.java
@@ -1,11 +1,13 @@
 package git.jackwisdom.sponge.securelogin.listener;
 
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.action.InteractEvent;
 import org.spongepowered.api.event.command.SendCommandEvent;
 import org.spongepowered.api.event.entity.DamageEntityEvent;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
 import org.spongepowered.api.event.entity.TargetEntityEvent;
+import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.item.inventory.ClickInventoryEvent;
 import org.spongepowered.api.event.item.inventory.DropItemEvent;
 import org.spongepowered.api.event.item.inventory.UseItemStackEvent;
@@ -21,47 +23,47 @@ public class AntiGriefListener {
 
     //chat
     @Listener
-    public void onPostCmd(SendCommandEvent event) {
+    public void onPostCmd(SendCommandEvent event, @First Player player) {
 
     }
 
     @Listener
-    public void onChat(MessageChannelEvent.Chat event) {
+    public void onChat(MessageChannelEvent.Chat event, @First Player player) {
     }
 
     @Listener
-    public void onMove(MoveEntityEvent event) {
+    public void onMove(MoveEntityEvent event, @First Player player) {
 
     }
 
     //inv
     @Listener
-    public void onDropItem(DropItemEvent event) {
+    public void onDropItem(DropItemEvent event, @First Player player) {
 
     }
 
     @Listener
-    public void onConsueItem(UseItemStackEvent event) {
+    public void onConsueItem(UseItemStackEvent event, @First Player player) {
 
     }
 
     @Listener
-    public void onClickInv(ClickInventoryEvent event) {
+    public void onClickInv(ClickInventoryEvent event, @First Player player) {
     }
 
     //interact
     @Listener
-    public void onInteractItem(InteractEvent e) {
+    public void onInteractItem(InteractEvent e, @First Player player) {
     }
 
     //Entity
     @Listener
-    public void onPlayerFight(DamageEntityEvent event) {
+    public void onPlayerFight(DamageEntityEvent event, @First Player player) {
 
     }
 
     @Listener
-    public void onEntityTarget(TargetEntityEvent event) {
+    public void onEntityTarget(TargetEntityEvent event, @First Player player) {
 
     }
 }
